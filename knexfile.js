4b84635d3282c1070210491d9dc425417254ab0b
Chris Leggatt <twoshow1978@gmail.com>
2023-08-15 14:38:01 -0400

    added initial file/folder structure; installed dependencies

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'rootroot',
+      database: 'capstone',
+      charset: 'utf8',
+    },
+  },
+};
