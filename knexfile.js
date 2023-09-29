9f274de27871779677aac00c3e47d04a8efe6443
Chris Leggatt <twoshow1978@gmail.com>
2023-09-29 02:44:15 -0400

    hotfix; adj knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,36 @@
 require('dotenv').config();
 
 module.exports = {
-  client: 'mysql',
-  connection: {
-    host: process.env.DATABASE_URL,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
-    database: process.env.DB_LOCAL_DBNAME,
-    charset: 'utf8'
+  development: {
+    host: process.env.DB_HOST,
+    connection: {
+      host: '127.0.0.1', 
+      port: 3306, 
+      user: process.env.DB_LOCAL_USER,
+      password: process.env.DB_LOCAL_PASSWORD,
+      database: process.env.DB_LOCAL_DBNAME,
+      charset: 'utf8'
+    }
   },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: 25060,
+      user: process.env.DB_LOCAL_USER,
+      password: process.env.DB_LOCAL_PASSWORD,
+      database: process.env.DB_LOCAL_DBNAME,
+      charset: 'utf8',
+      ssl: true
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 };
+
