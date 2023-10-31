29b855f27b17ca28a48eabd11e211447cd149255
Chris Leggatt <twoshow1978@gmail.com>
2023-10-31 19:04:13 -0400

    hotfix; db migration/seed config

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
     client: 'mysql',
     connection: {
       host: process.env.DB_HOST,
-      port: 25060,
+      port: 3306,
       user: process.env.DB_LOCAL_USER,
       password: process.env.DB_LOCAL_PASSWORD,
       database: process.env.DB_LOCAL_DBNAME,
@@ -31,6 +31,9 @@ module.exports = {
     },
     migrations: {
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
   }
 };
